a9072b6044d5247512568b0c389cb3faf5f6af14
JoeTwizzle <the3.14159268483279@gmail.com>
2024-02-21 07:13:38 +0100

    Add Web Browser for cartavis

---
+++ Assets/BrowserManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VoltstroStudios.UnityWebBrowser;
+
+public class BrowserManager : MonoBehaviour
+{
+    public WebBrowserUIBasic Browser;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
